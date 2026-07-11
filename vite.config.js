41042596ac384aa1298bd5829c9d7f7f4247dc65
HarshankPatel9038 <patelharshank8@gmail.com>
2026-07-11 14:10:25 +0530

    Update config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PokeVerse-Explorer/',
+  base: process.env.VERCEL ? '/' : '/PokeVerse-Explorer/',
 })
